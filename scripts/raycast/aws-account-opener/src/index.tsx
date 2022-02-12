6395c7376e213c6469639f237ac149da7caeb38a
Andre Biel <andre@no.studio>
2022-02-12 13:58:30 +0100

    add correct profiles

--- scripts/raycast/aws-account-opener/src/index.tsx
+++ scripts/raycast/aws-account-opener/src/index.tsx
@@ -5,36 +5,64 @@ import { useEffect, useState } from "react";
 
 const CONFIG = `${os.homedir}/.aws/config`;
 
+interface AwsCredential {
+  sso_start_url?: string;
+  sso_region?: string;
+  sso_account_id?: string;
+  sso_role_name?: string;
+  region?: string;
+  output?: string;
+}
+type StateType = Record<string, AwsCredential>;
+
 export default function Command() {
-  const [profiles, setProfiles] = useState<string[]>([]);
+  const [profiles, setProfiles] = useState<StateType>({});
 
   useEffect(() => {
     const buffer = fs.readFileSync(CONFIG);
-    const s = buffer.toString();
-    let result = [],
-      m,
-      rx = /\[profile (.*?)\]/g;
+    const fileContent = buffer.toString().split(/\r?\n/);
+    let currentParent = "";
+    const result: StateType = {};
+    fileContent.forEach((line) => {
+      const trimmedLine = line.trim();
 
-    while ((m = rx.exec(s)) !== null) {
-      result.push(m[1]);
-    }
+      if (trimmedLine.startsWith("[") && trimmedLine.endsWith("]")) {
+        const profileRegex = new RegExp(/\[profile(.*)\]/gm);
+        const match = profileRegex.exec(trimmedLine);
+        const profileName = match?.[1]?.trim() || trimmedLine;
+        currentParent = profileName;
+        result[profileName] = {};
+        return;
+      }
 
-    setProfiles(result.sort());
+      const splitted = line.split("=");
+      const identifier = splitted[0]?.trim() as keyof AwsCredential;
+      const value = splitted[1]?.trim();
+
+      if (value && identifier) {
+        result[currentParent][identifier] = value;
+      }
+    });
+
+    setProfiles(result);
   }, []);
 
-  const renderProfile = (item: string, index: number) => {
+  const renderProfile = (profile: keyof StateType, index: number) => {
+    const cred = profiles[profile];
+
     return (
       <ListItem
         key={index}
-        title={item}
+        title={profile}
+        subtitle={cred?.sso_account_id}
         actions={
           <ActionPanel>
-            <CopyToClipboardAction content={item} />
+            <CopyToClipboardAction content={`export AWS_PROFILE=${profile}`} />
           </ActionPanel>
         }
       />
     );
   };
 
-  return <List>{profiles.map(renderProfile)}</List>;
+  return <List>{Object.keys(profiles).map(renderProfile)}</List>;
 }
